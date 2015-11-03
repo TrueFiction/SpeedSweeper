87add782b2b458adcc75cdaf15e94552bc5722f2
sbraaten95 <sbraaten95@gmail.com>
2015-11-02 19:42:27 -0800

    It is now working quite well, at the expense of sloppy coding. I am now going to start working on what happens if a tile without a bomb is clicked. It expands and displays tiles that indicate how many bombs are around.

--- SpeedSweeper/app/src/main/java/com/app/speedsweeper/speedsweeper/GameActivity.java
+++ SpeedSweeper/app/src/main/java/com/app/speedsweeper/speedsweeper/GameActivity.java
@@ -9,29 +9,90 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
-
     /*
-    Getting some tough to solve errors by calling the methods below.
-    I am trying to populate an arraylist with view objects, each one
-    being of the form of the XML made buttons I labelled with separate
-    ids. I am doing this so that I can access all the buttons individually,
-    allowing me to place the bombs.
+    Alrighty then, I have a good framework. I now need to figure out how to expand tiles
+        and create the minesweeper gameplay.
+
+    Other tasks:
+        Main menu
+        High score
+        Timer
+        Proper graphics design
      */
 
     View reset;
     ArrayList<View> tiles = new ArrayList<>();
     ArrayList<Integer> originalIds = new ArrayList<>();
     Random r = new Random();
+    Boolean check = true;
 
     public void initializeGrid(){
         grabChildren();
         saveId();
         plantBombs();
     }
+    /*
+    I am not at all proud of the following method, but at least after this,
+        I have exactly what I want. Note that I used eclipse to generate
+        the below code in String output.
+     */
     public void grabChildren(){
-        View view;
-        view = findViewById(R.id.mine_field);
-        ((ViewGroup) view).addChildrenForAccessibility(tiles);
+        tiles.add(findViewById(R.id.tile_1));
+        tiles.add(findViewById(R.id.tile_2));
+        tiles.add(findViewById(R.id.tile_3));
+        tiles.add(findViewById(R.id.tile_4));
+        tiles.add(findViewById(R.id.tile_5));
+        tiles.add(findViewById(R.id.tile_6));
+        tiles.add(findViewById(R.id.tile_7));
+        tiles.add(findViewById(R.id.tile_8));
+        tiles.add(findViewById(R.id.tile_9));
+        tiles.add(findViewById(R.id.tile_10));
+        tiles.add(findViewById(R.id.tile_11));
+        tiles.add(findViewById(R.id.tile_12));
+        tiles.add(findViewById(R.id.tile_13));
+        tiles.add(findViewById(R.id.tile_14));
+        tiles.add(findViewById(R.id.tile_15));
+        tiles.add(findViewById(R.id.tile_16));
+        tiles.add(findViewById(R.id.tile_17));
+        tiles.add(findViewById(R.id.tile_18));
+        tiles.add(findViewById(R.id.tile_19));
+        tiles.add(findViewById(R.id.tile_20));
+        tiles.add(findViewById(R.id.tile_21));
+        tiles.add(findViewById(R.id.tile_22));
+        tiles.add(findViewById(R.id.tile_23));
+        tiles.add(findViewById(R.id.tile_24));
+        tiles.add(findViewById(R.id.tile_25));
+        tiles.add(findViewById(R.id.tile_26));
+        tiles.add(findViewById(R.id.tile_27));
+        tiles.add(findViewById(R.id.tile_28));
+        tiles.add(findViewById(R.id.tile_29));
+        tiles.add(findViewById(R.id.tile_30));
+        tiles.add(findViewById(R.id.tile_31));
+        tiles.add(findViewById(R.id.tile_32));
+        tiles.add(findViewById(R.id.tile_33));
+        tiles.add(findViewById(R.id.tile_34));
+        tiles.add(findViewById(R.id.tile_35));
+        tiles.add(findViewById(R.id.tile_36));
+        tiles.add(findViewById(R.id.tile_37));
+        tiles.add(findViewById(R.id.tile_38));
+        tiles.add(findViewById(R.id.tile_39));
+        tiles.add(findViewById(R.id.tile_40));
+        tiles.add(findViewById(R.id.tile_41));
+        tiles.add(findViewById(R.id.tile_42));
+        tiles.add(findViewById(R.id.tile_43));
+        tiles.add(findViewById(R.id.tile_44));
+        tiles.add(findViewById(R.id.tile_45));
+        tiles.add(findViewById(R.id.tile_46));
+        tiles.add(findViewById(R.id.tile_47));
+        tiles.add(findViewById(R.id.tile_48));
+        tiles.add(findViewById(R.id.tile_49));
+        tiles.add(findViewById(R.id.tile_50));
+        tiles.add(findViewById(R.id.tile_51));
+        tiles.add(findViewById(R.id.tile_52));
+        tiles.add(findViewById(R.id.tile_53));
+        tiles.add(findViewById(R.id.tile_54));
+        tiles.add(findViewById(R.id.tile_55));
+        tiles.add(findViewById(R.id.tile_56));
     }
     private void saveId(){
         for (int x = 0; x < tiles.size(); x++){
@@ -39,32 +100,39 @@ public class GameActivity extends AppCompatActivity {
         }
     }
     public void plantBombs(){
-        for (int x = 1; x <= 10; x++) {
-            View unluckyTile = tiles.get(1);
+        for (int x = 0; x < 7; x++) {
+            View unluckyTile = tiles.get(r.nextInt(tiles.size()));
             unluckyTile.setId(R.id.bomb);
         }
     }
-    public Boolean checkTile(View v, Boolean b){
+    public Boolean checkTile(View v){
         if (v.getId() == R.id.bomb){
-            b = explosionResult();
+            Boolean b = explosionResult();
             return b;
         } else {
-            b = safeResult();
+            Boolean b = safeResult();
             return b;
         }
     }
     public Boolean explosionResult(){
-        return false;
-    }
-    public Boolean safeResult(){
         return true;
     }
+    public Boolean safeResult(){
+        return false;
+    }
+    /*
+    In the following method I would reset game data.
+     */
     public void resetGame(View v){
         for (int x = 0; x < tiles.size(); x++) {
             View Tile = tiles.get(x);
             Tile.setId(originalIds.get(x));
             Tile.setBackgroundResource(R.drawable.temp_pic2);
         }
+        plantBombs();
+        reset.setVisibility(View.INVISIBLE);
+        reset.setClickable(false);
+        check = true;
     }
     /*
     Normal creation.
@@ -89,17 +157,19 @@ public class GameActivity extends AppCompatActivity {
         conditional test in there but just to test its reliability.
      */
     public void registerClick(View v){
-        Boolean entry = false;
-        entry = checkTile(v, entry);
-        if (entry) {
-            endGame(v);
+        if (check) {
+            Boolean entry;
+            entry = checkTile(v);
+            if (entry) {
+                endGame(v);
+            }
         }
     }
     /*
-    Ideally I would use many methods like this to allow the
-        computer to manage all possible outcomes.
+    In the following method I would save game data.
      */
     public void endGame(View v) {
+        check = false;
         v.setBackgroundResource(R.drawable.bomb_1);
         reset.setVisibility(View.VISIBLE);
         reset.setClickable(true);
